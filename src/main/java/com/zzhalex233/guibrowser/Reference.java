d37a9488c60e136b1bb3dce5207790972b495ea7
zzhalex233 <zzhalex@outlook.com>
2026-05-05 11:21:49 +0800

    update version number

--- src/main/java/com/zzhalex233/guibrowser/Reference.java
+++ src/main/java/com/zzhalex233/guibrowser/Reference.java
@@ -3,7 +3,7 @@ package com.zzhalex233.guibrowser;
 public final class Reference {
     public static final String MOD_ID = "guibrowser";
     public static final String MOD_NAME = "GUI Browser";
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "1.0";
 
     private Reference() {
     }
